c002217aa5e9fcbb558eb4c87193139d055a71e0
Liju Robin George <liju.leelives@gmail.com>
2016-11-21 00:02:00 -0500

    added panels

--- js/script.js
+++ js/script.js
@@ -142,7 +142,7 @@ function createReport(headers) {
 function printPage() {
     window.print();
 }
-//The function to sorth the Table
+//The function to sort the Table
 function sortTable(tbody, col, asc) {
     var rows = tbody.rows,
         rlen = rows.length,
@@ -250,22 +250,14 @@ function Statistics(col) {
 	createCharts();
 }
 
-
 function createCharts() {
-	
-	d3.csv("./data/MOCK_DATA.csv", function(error, disasterData) {
-		
-		console.log(disasterData);
-		// normalize/parse data
-		disasterData.forEach(function(d) {
-			d.Spent = d.Spent.match(/\d+/);
-			console.log(d.Spent);
-		});
+	console.log("Hello");
+	d3.csv("./data/quakes.csv", function(error, disasterData) {
 		
 		// format our data
 		var dtgFormat = d3.time.format("%Y-%m-%dT%H:%M:%S");
 		
-		 data.forEach(function(d) { 
+		 disasterData.forEach(function(d) { 
 			d.dtg   = dtgFormat.parse(d.origintime.substr(0,19)); 
 			d.lat   = +d.latitude;
 			d.long  = +d.longitude;
@@ -276,7 +268,93 @@ function createCharts() {
 		var magnitudeChart = dc.barChart("#dc-magnitude-chart");
 		var depthChart = dc.barChart("#dc-depth-chart");
 		var timeChart = dc.lineChart("#dc-time-chart");
-	
-	});
-	
+		
+		var facts = crossfilter(disasterData);
+		
+		// for Magnitude
+		  var magValue = facts.dimension(function (d) {
+			return d.mag;       // group or filter by magnitude
+		  });
+		  var magValueGroupSum = magValue.group()
+			.reduceSum(function(d) { return d.mag; });	// sums the magnitudes per magnitude
+		  var magValueGroupCount = magValue.group()
+			.reduceCount(function(d) { return d.mag; }) // counts the number of the facts by magnitude
+
+		  // For datatable
+		  var timeDimension = facts.dimension(function (d) {
+			return d.dtg;
+		  }); // group or filter by time
+
+		  // for Depth
+		  var depthValue = facts.dimension(function (d) {
+			return d.depth;
+		  });
+		  var depthValueGroup = depthValue.group();
+		  
+		  // define a daily volume Dimension
+		  var volumeByDay = facts.dimension(function(d) {
+			return d3.time.hour(d.dtg);
+		  });
+		  // map/reduce to group sum
+		  var volumeByDayGroup = volumeByDay.group()
+			.reduceCount(function(d) { return d.dtg; });
+
+		// Magnitide Bar Graph Summed
+		  magnitudeChart.width(480)
+			.height(150)
+			.margins({top: 10, right: 10, bottom: 20, left: 40})
+			.dimension(magValue)								// the values across the x axis
+			.group(magValueGroupSum)							// the values on the y axis
+			.transitionDuration(500)
+			.centerBar(true)	
+			.gap(56)                                            // bar width Keep increasing to get right then back off.
+			.x(d3.scale.linear().domain([0.5, 7.5]))
+			.elasticY(true)
+			.xAxis().tickFormat(function(v) {return v;});	
+
+		// Depth bar graph
+		  depthChart.width(480)
+			.height(150)
+			.margins({top: 10, right: 10, bottom: 20, left: 40})
+			.dimension(depthValue)
+			.group(depthValueGroup)
+			.transitionDuration(500)
+			.centerBar(true)	
+			.gap(1)                    // bar width Keep increasing to get right then back off.
+			.x(d3.scale.linear().domain([0, 100]))
+			.elasticY(true)
+			.xAxis().tickFormat(function(v) {return v;});
+
+		// time graph
+		  timeChart.width(960)
+			.height(100)
+			.margins({top: 10, right: 10, bottom: 20, left: 40})
+			.dimension(volumeByDay)
+			.group(volumeByDayGroup)
+			.transitionDuration(500)
+			.elasticY(true)
+			.x(d3.time.scale().domain([new Date(2013, 6, 18), new Date(2013, 6, 24)])) // scale and domain of the graph
+			.xAxis();
+
+		 // Table of earthquake data
+		 /*
+		  dataTable.width(960).height(800)
+			.dimension(timeDimension)
+			.group(function(d) { return "List of all earthquakes corresponding to the filters"
+			 })
+			.size(10)							// number of rows to return
+			.columns([
+			  function(d) { return d.dtg; },
+			  function(d) { return d.lat; },
+			  function(d) { return d.long; },
+			  function(d) { return d.depth; },
+			  function(d) { return d.mag; },
+			  function(d) { return '<a href=\"http://maps.google.com/maps?z=11&t=m&q=loc:' + d.lat + '+' + d.long +"\" target=\"_blank\">Google Map</a>"},
+			  function(d) { return '<a href=\"http://www.openstreetmap.org/?mlat=' + d.lat + '&mlon=' + d.long +'&zoom=11'+ "\" target=\"_blank\"> OSM Map</a>"}
+			])
+			.sortBy(function(d){ return d.dtg; })
+			.order(d3.ascending); */
+			
+		dc.renderAll();
+  });
 }
